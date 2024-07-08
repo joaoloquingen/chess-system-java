7f7cfc1f48da8b002eb1784f839479893a3bfa6d
João Loquingen <vichtor_jovem@hotmail.com>
2024-07-08 02:21:06 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,15 @@
 package application;
 
+import boardgame.Position;
+
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
